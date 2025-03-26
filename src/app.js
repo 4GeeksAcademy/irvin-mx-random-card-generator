44904ee714cae05e2464a6d1e540dacd34c70fbd
Irvin Martinez <100785904+Irvin-Mx@users.noreply.github.com>
2025-03-26 22:14:22 +0000

    Finished random card generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const iconsObjectsArrays = [{icon:"♦",color:"red"},{icon:"♥",color:"red"},{icon:"♠",color:"black"},{icon:"♣",color:"black"}]
+
+  let randomNumberSuite = Math.floor( Math.random() * iconsObjectsArrays.length )
+
+  let randomNumberCard = Math.floor( Math.random() * 12 )
+
+  let iconObjectWinner = iconsObjectsArrays[randomNumberSuite]
+
+  let iconWinner = iconObjectWinner.icon
+
+  let spanNumberElement = document.getElementById("random-number")
+  let iconSpansNodeList = document.querySelectorAll(".icon-span")
+
+  for(let spanNode of iconSpansNodeList){
+    spanNode.style.color = iconObjectWinner.color
+    spanNode.style.fontSize= "100px"
+    spanNode.innerHTML = iconWinner
+   }
+
+   spanNumberElement.innerHTML = randomNumberCard
+
 };
